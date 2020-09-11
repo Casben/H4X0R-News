0d180619780fc0b115c862553f67a73d77c9adbe
HDodge <herbiedodge@gmail.com>
2020-09-10 22:51:44 -0500

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Herbert Dodge on 9/10/20.
+//  Copyright © 2020 Herbert Dodge. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
